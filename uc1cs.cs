a0f5547e9f4ad2810a8bda0fdd5ca1de1261bdc3
ErElectical <mohittanwar786.mt@gmail.com>
2021-12-24 07:45:58 +0000

    employee present or not

---
+++ uc1cs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeewagesproblem
+{
+    public  class uc1
+    {
+        static void Employeecheck()
+        {
+            Random random= new Random();
+            int check = random.Next(0, 2);
+            if (check == 0)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is not present");
+
+            }
+
+        }
+        
+    }
+}
