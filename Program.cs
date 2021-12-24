e42d69cccbaedfbfc383d54169e7277e1261c5d2
ErElectical <mohittanwar786.mt@gmail.com>
2021-12-24 04:13:49 +0000

    welcome to master branch

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Employeewages
+{
+    public class program
+    {
+        static void Main(string[]  args)
+        {
+            Console.WriteLine("welcome to employee wages computation on master barnch ");
+        }
+    }
+}
