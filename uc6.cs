e512d2c16c2783180d190f2bb35559bc5b324b03
ErElectical <mohittanwar786.mt@gmail.com>
2021-12-25 04:11:58 +0000

    employee wages till working hour 100

---
+++ uc6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeewagesproblem
+{
+    public  class uc6
+    {
+       public static void employeemaxhour()
+        {
+            int employeemaxhour = 100;
+            int employeehour = 0;
+            
+            while(employeehour < employeemaxhour)
+            {
+                employeehour += uc5.employeewagesmonth();
+            }
+        }
+
+
+    }
+}
